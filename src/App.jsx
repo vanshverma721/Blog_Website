078057325fc3f4b5d1ae62524a2fdd992ca44d3a
vanshverma721 <vvv.721781@gmail.com>
2024-01-23 19:57:05 +0530

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import './App.css'
 
 function App() {
-
+  
   return (
     <>
       <h1 className='text-red-500'>Hello World</h1>
---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: "fir-blog-79025.firebaseapp.com",
+    projectId: "fir-blog-79025",
+    storageBucket: "fir-blog-79025.appspot.com",
+    messagingSenderId: "445752664843",
+    appId: "1:445752664843:web:b8873094ec7931d84a780e",
+    measurementId: "G-LDCL0L6PF5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+export const storage = getStorage();
+export const db = getFirestore(app);
