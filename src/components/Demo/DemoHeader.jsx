69253b9dd9dff8e9ca2d81b32a4205a8e57d7f2b
vanshverma721 <vvv.721781@gmail.com>
2024-01-23 20:58:14 +0530

    added the code

---
+++ src/components/Demo/DemoHeader.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { nav } from '../../data';
+
+const DemoHeader = () => {
+    return (
+        <header className='border-b border-black sticky top-0 z-50'>
+            <div className='size h-[70px] flex items-center justify-between'>
+                <Link to={"/"}>
+                    <img
+                        className="h-[2.5rem]"
+                        src="https://miro.medium.com/v2/resize:fit:8978/1*s986xIGqhfsN8U--09_AdA.png"
+                        alt="logo"
+                    />
+                </Link>
+                <div className='flex items-center gap-5'>
+                    <div className='hidden text-sm sm:flex items-center gap-5'>
+                        {nav.map((link, i) => (
+                            <Link key={i} to={link.path}>{link.title}</Link>
+                        ))}
+                    </div>
+                    <div className='relative'>
+                        <button className='hidden text-sm sm:flex items-center gap-5'>Sign In</button>
+                    </div>
+                    <button className='bg-black text-white rounded-full px-3 p-2 text-sm font-medium'>Get Started</button>
+                </div>
+
+            </div>
+        </header>
+    )
+}
+
+export default DemoHeader
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black1: "rgba(0,0,0,0.8)",
+        banner: "rgb(255, 192, 23)",
+      },
+      fontFamily: {
+        title: `gt-super, Georgia, Cambria, Times New Roman, Times, serif:`,
+        texts: `sohne, Helvetica Neue, Helvetica, Arial, sans-serif`,
+      },
+      gridTemplateColumns: {
+        card: "repeat(auto-fit, minmax(280px, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
+};
