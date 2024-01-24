e884e7823c3a54c63587bb923ba4ec953a03310d
vanshverma721 <vvv.721781@gmail.com>
2024-01-24 20:56:13 +0530

    code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Home from "./components/Home/Home"
 import Demo from "./components/Demo/Demo"
 import HomeHeader from "./components/Home/HomeHeader";
@@ -6,14 +6,15 @@ import DemoHeader from "./components/Demo/DemoHeader";
 
 function App() {
 
-  const auth = false;
+  const currentUser = false;
 
   return (
     <>
-      {auth ? <HomeHeader /> : <DemoHeader />}
+      {currentUser ? <HomeHeader /> : <DemoHeader />}
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/demo" element={<Demo />} />
+        {currentUser && <Route path="/" element={<Home />} />}
+        {!currentUser && <Route path="/demo" element={<Demo />} />}
+        <Route path="*" element={<Navigate to={!currentUser ? "/demo" : "/"} />} />
       </Routes>
     </>
   )
