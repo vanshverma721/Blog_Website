e3e035d303d0a49568de1e42274c91d52499fc1e
vanshverma721 <vvv.721781@gmail.com>
2024-01-27 20:17:28 +0530

    code added

--- src/components/Common/Posts/Posts.jsx
+++ src/components/Common/Posts/Posts.jsx
@@ -36,6 +36,16 @@ const Posts = () => {
       <h1>Post</h1>
       <h1>Post</h1>
       <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
     </div>
   )
 }
