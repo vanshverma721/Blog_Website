04e3129a9050df01bc266aef8e39e2541ec605a3
vanshverma721 <vvv.721781@gmail.com>
2024-01-27 20:49:42 +0530

    updated

--- src/components/Common/Posts/Posts.jsx
+++ src/components/Common/Posts/Posts.jsx
@@ -46,6 +46,18 @@ const Posts = () => {
       <h1>Post</h1>
       <h1>Post</h1>
       <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
+      <h1>Post</h1>
     </div>
   )
 }
