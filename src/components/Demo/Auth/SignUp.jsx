8f9712678483851fe59cd9a7a7c44c88367f9314
vanshverma721 <vvv.721781@gmail.com>
2024-02-04 17:00:47 +0530

    Auth with email and password is added

--- src/components/Demo/Auth/SignUp.jsx
+++ src/components/Demo/Auth/SignUp.jsx
@@ -2,8 +2,15 @@ import React, { useState } from 'react'
 import Input from '../../../utils/Input'
 import { MdKeyboardArrowLeft } from "react-icons/md"
 import { toast } from 'react-toastify';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, db } from '../../../firebase/firebase';
+import { useNavigate } from 'react-router-dom';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
-const SignUp = ({ setSignReq }) => {
+const SignUp = ({ setSignReq, setModal }) => {
+
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   const [form, setForm] = useState({
     Username: "",
@@ -12,10 +19,37 @@ const SignUp = ({ setSignReq }) => {
     RePassword: "",
   });
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (form[("Username", "Email", "Password", "RePassword")] === "") {
       toast.error("All fields are required");
+    } else if (form["Password"] !== form["RePassword"]) {
+      toast.error("Your passwords are not matching!!");
+      return;
+    } else {
+      setLoading(true)
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        form.Email,
+        form.Password
+      );
+
+      const ref = doc(db, "users", user.uid);
+      const userDoc = await getDoc(ref);
+
+      if (!userDoc.exists()) {
+        await setDoc(ref, {
+          userId: user.uid,
+          username: form.Username,
+          email: form.Email,
+          userImg: "",
+          bio: "",
+        });
+        navigate("/");
+        toast.success("User have been Created");
+        setModal(false);
+        setLoading(false);
+      }
     }
   }
 
@@ -28,7 +62,7 @@ const SignUp = ({ setSignReq }) => {
         <Input form={form} setForm={setForm} type="email" title="Email" />
         <Input form={form} setForm={setForm} type="password" title="Password" />
         <Input form={form} setForm={setForm} type="password" title="RePassword" />
-        <button className='px-4 py-1 text-sm rounded-full bg-green-700 hover:bg-green-800 text-white w-fit mx-auto'>Sign Up</button>
+        <button className={`px-4 py-1 text-sm rounded-full bg-green-700 hover:bg-green-800 text-white w-fit mx-auto ${loading ? "opacity-50 pointer-events-none" : ""}`}>Sign Up</button>
       </form>
       <button onClick={() => setSignReq("")} className='mt-5 text-sm text-green-600 hover:text-green-700 flex items-center mx-auto'> <MdKeyboardArrowLeft /> All sign Up Options</button>
     </div>
