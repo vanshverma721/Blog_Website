03140921226a01378b129185395a324a26c47196
vanshverma721 <vvv.721781@gmail.com>
2024-02-05 00:02:08 +0530

    search bar has been added

--- src/components/Home/Header/Search.jsx
+++ src/components/Home/Header/Search.jsx
@@ -3,13 +3,13 @@ import { CiSearch } from "react-icons/ci";
 import Modal from '../../../utils/Modal';
 
 const Search = ({ modal, setModal }) => {
-  
+
   const hidden = modal ? "visible opacity-100" : "invisible opacity-0";
 
   return (
     <>
       <Modal modal={modal} setModal={setModal} hidden={hidden} >
-        <div className={`absolute sm:relative right-4 left-4 top-[4rem] sm:left-0 sm:top-0 ${modal ? "visible opacity-100%" : "invisible opacity-0"} transition-all duration-200`}>
+        <div className={`absolute sm:relative right-4 left-4 top-[4rem] sm:left-0 sm:top-0 ${modal ? "visible opacity-100%" : "invisible sm:visible sm:opacity-100 opacity-0"} transition-all duration-200`}>
           <div className="flex items-center gap-1 bg-gray-100 px-2 rounded-full relative z-10">
             <span className="text-2xl text-gray-400">
               <CiSearch />
