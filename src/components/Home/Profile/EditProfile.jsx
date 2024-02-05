69fe305ce47780c6c306806d2176d89de238e2c1
vanshverma721 <vvv.721781@gmail.com>
2024-02-05 19:45:11 +0530

    profile update button added

--- src/components/Home/Profile/EditProfile.jsx
+++ src/components/Home/Profile/EditProfile.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Modal from '../../../utils/Modal';
 import { LiaTimesSolid } from "react-icons/lia";
 
 const EditProfile = ({ editModal, setEditModal }) => {
+
+    const imgref = useRef(null);
+
+    const openfile = () => {
+        imgref.current.click();
+    }
+
     return (
         <Modal modal={editModal} setModal={setEditModal}>
             <div className="center w-[95%] md:w-[45rem] bg-white mx-auto shadows my-[1rem] z-20 mb-[3rem] p-[2rem]">
@@ -22,10 +29,11 @@ const EditProfile = ({ editModal, setEditModal }) => {
                     <div className="flex gap-[2rem]">
                         <div className="w-[5rem]">
                             <img className="min-h-[5rem] min-w-[5rem] object-cover border-gray-400 rounded-full" src="/profile.png" alt="profile-img" />
+                            <input accept="image/png, image/jpeg, image/jpg" ref={imgref} type="file" hidden />
                         </div>
                         <div>
                             <div className="flex gap-4 text-sm">
-                                <button className="text-green-600">Update</button>
+                                <button onClick={openfile} className="text-green-600">Update</button>
                                 <button className="text-red-600">Remove</button>
                             </div>
                             <p className="w-full sm:w-[20rem] text-gray-500 pt-2">Recommended: Square JPG, PNG or GIF at least 1,000 pixels per side.</p>
